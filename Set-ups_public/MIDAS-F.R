2cfccba058ded1ca39732ce8f31c855102f69a85
Jasper de Winter <J.M.de.Winter@dnb.nl>
2024-05-16 16:55:51 +0200

    MIDAS-F updated

--- Set-ups_public/MIDAS-F.R
+++ Set-ups_public/MIDAS-F.R
@@ -3,6 +3,10 @@
 #     Dennis Kant, Andreas Pick and Jasper de Winter
 #     May 13, 2024
 #
+#     WARNING: The outcome is based on raw data to estimate the MIDAS-F based on 
+#     a dynamic factor model with 1 static factor, 1 dynamic factor and 1 lag
+#     in the vector auto regressive part of the model.
+#
 ################################################################################
 
 ######################### Estimation ###########################################
